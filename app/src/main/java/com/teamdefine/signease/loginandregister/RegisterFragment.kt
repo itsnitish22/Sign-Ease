65c0d2b953eaf2b48d389765c852c53d6582e9a8
itsnitish22 <nitish.sharma1186@gmail.com>
2022-09-11 22:43:15 +0530

    registering user work done

--- app/src/main/java/com/teamdefine/signease/loginandregister/RegisterFragment.kt
+++ app/src/main/java/com/teamdefine/signease/loginandregister/RegisterFragment.kt
@@ -1,17 +1,55 @@
 package com.teamdefine.signease.loginandregister
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.teamdefine.signease.R
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.teamdefine.signease.databinding.FragmentRegisterBinding
 
 class RegisterFragment : Fragment() {
+    private lateinit var binding: FragmentRegisterBinding
+    private lateinit var auth: FirebaseAuth //firebase auth
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding = FragmentRegisterBinding.inflate(inflater, container, false)
+
+        binding.registerButton.setOnClickListener {
+            val fullName = binding.editTextTextPersonFullName.text.toString()
+            val uid = binding.editTextUID.text.toString()
+            val email = binding.editTextEmail.text.toString().trim()
+            val password = binding.editTextPassword.text.toString()
+            registerUser(fullName, uid, email, password)
+
+        }
+        return binding.root
+    }
+
+    private fun registerUser(
+        fullName: String,
+        uid: String,
+        email: String,
+        password: String
+    ) {
+        auth = FirebaseAuth.getInstance()
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+            OnCompleteListener { task ->
+                if (task.isSuccessful)
+                    Toast.makeText(activity, "User Registered", Toast.LENGTH_SHORT).show()
+                else
+                    Toast.makeText(
+                        activity,
+                        task.exception!!.message.toString(),
+                        Toast.LENGTH_LONG
+                    ).show()
+            }
+        )
     }
 }
