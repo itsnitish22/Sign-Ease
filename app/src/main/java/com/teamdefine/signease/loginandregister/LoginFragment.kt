cec6f142dca9804c38958e29d6bdfef9041db988
itsnitish22 <nitish.sharma1186@gmail.com>
2022-09-12 14:22:08 +0530

    logging in functionality done, formatted code

--- app/src/main/java/com/teamdefine/signease/loginandregister/LoginFragment.kt
+++ app/src/main/java/com/teamdefine/signease/loginandregister/LoginFragment.kt
@@ -4,17 +4,39 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
 import com.teamdefine.signease.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.editTextEmail.text.toString().trim()
+            val password = binding.editTextPassword.text.toString()
+            loginUser(email, password)
+        }
         return binding.root
+    }
+
+    private fun loginUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful)
+                Toast.makeText(activity, "Logged In", Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(
+                    activity,
+                    task.exception!!.message.toString(),
+                    Toast.LENGTH_LONG
+                ).show()
+        }
     }
 }
