9bb5a937b1508f3f2b5827d646ef6d6b3240c103
itsnitish22 <nitish.sharma1186@gmail.com>
2022-09-18 12:24:21 +0530

    added comments in login fragment

--- app/src/main/java/com/teamdefine/signease/loginandregister/LoginFragment.kt
+++ app/src/main/java/com/teamdefine/signease/loginandregister/LoginFragment.kt
@@ -6,44 +6,51 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.teamdefine.signease.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
-    private lateinit var binding: FragmentLoginBinding
-    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var binding: FragmentLoginBinding //binding
+    private lateinit var firebaseAuth: FirebaseAuth //firebase auth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
-        firebaseAuth = FirebaseAuth.getInstance()
+        firebaseAuth = FirebaseAuth.getInstance() //getting instance
+
+        //on click of login button, login the user
         binding.loginButton.setOnClickListener {
             val email = binding.inputEmail.text.toString()
             val password = binding.inputPassword.text.toString()
-            loginUser(email, password)
+
+            if (email.isEmpty() || password.isEmpty())
+                Toast.makeText(activity, "All fields are mandatory", Toast.LENGTH_SHORT).show()
+            else {
+                binding.progressBar.visibility = View.VISIBLE
+                loginUser(email, password)
+            }
         }
         return binding.root
     }
 
+    //login user function
     private fun loginUser(email: String, password: String) {
         firebaseAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener(
-                OnCompleteListener<AuthResult> { task ->
-                    if (task.isSuccessful) {
-                        Toast.makeText(activity, "Logged In", Toast.LENGTH_SHORT)
-                            .show()
-                    } else {
-                        Toast.makeText(
-                            activity,
-                            task.exception!!.message.toString(),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(activity, "Logged In", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    binding.progressBar.visibility = View.GONE
+                    Toast.makeText(
+                        activity,
+                        task.exception!!.message.toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
-            )
+            }
     }
 }
