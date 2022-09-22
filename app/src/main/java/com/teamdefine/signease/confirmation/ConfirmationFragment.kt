baadfe6584cdbcb2ba2f7db00bc401f4f926b88c
itsnitish22 <nitish.sharma1186@gmail.com>
2022-09-22 16:27:58 +0530

    aiithe rakhh! confirmation frag designed

--- app/src/main/java/com/teamdefine/signease/confirmation/ConfirmationFragment.kt
+++ app/src/main/java/com/teamdefine/signease/confirmation/ConfirmationFragment.kt
@@ -2,20 +2,18 @@ package com.teamdefine.signease.confirmation
 
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.teamdefine.signease.R
 import com.teamdefine.signease.api.models.post_template_for_sign.Document
 import com.teamdefine.signease.databinding.FragmentConfirmationBinding
-import com.teamdefine.signease.templates.URLs
 
 
 class ConfirmationFragment : Fragment() {
-
     private lateinit var viewModel: ConfirmationViewModel
     private lateinit var binding: FragmentConfirmationBinding
 
@@ -34,16 +32,47 @@ class ConfirmationFragment : Fragment() {
 
 
         //web view stuff
-        val webview = binding.webView
-        webview.settings.javaScriptEnabled = true
-        val pdf =
-            "https://firebasestorage.googleapis.com/v0/b/sign-ease.appspot.com/o/DL.pdf?alt=media&token=863e24b4-fd59-496c-ab63-7e3fb78a6476"
-        webview.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=$pdf")
-        val pdf2 = URLs.getUrlsFromData()["Application For Duty Leave"]
-        Log.i("Confirm Frag LULLA", pdf2.toString())
+//        val webview = binding.webView
+//        webview.settings.javaScriptEnabled = true
+//        val pdf =
+//            "https://firebasestorage.googleapis.com/v0/b/sign-ease.appspot.com/o/DL.pdf?alt=media&token=863e24b4-fd59-496c-ab63-7e3fb78a6476"
+//        webview.loadUrl("https://drive.google.com/viewerng/viewer?embedded=true&url=$pdf")
+//        val pdf2 = URLs.getUrlsFromData()["Application For Duty Leave"]
+//        Log.i("Confirm Frag LULLA", pdf2.toString())
 
+        binding.confirmButton.isClickable = false
+        binding.confirmButton.isEnabled = false
+        binding.checkBox.setOnClickListener {
+            if (binding.checkBox.isChecked) {
+                binding.confirmButton.isClickable = true
+                binding.confirmButton.isEnabled = true
+                binding.confirmButton.setBackgroundColor(
+                    binding.confirmButton.context.resources.getColor(
+                        R.color.blue
+                    )
+                )
+            } else {
+                binding.confirmButton.isClickable = false
+                binding.confirmButton.isEnabled = false
+                binding.confirmButton.setBackgroundColor(
+                    binding.confirmButton.context.resources.getColor(
+                        R.color.grey
+                    )
+                )
+            }
+        }
 
+        binding.changeTemplateText.setOnClickListener {
+            binding.progressBar.visibility = View.VISIBLE
+            findNavController().navigate(ConfirmationFragmentDirections.actionConfirmationFragmentToTemplateFragment())
+            binding.progressBar.visibility = View.GONE
+        }
 
+        binding.changeDateText.setOnClickListener {
+            binding.progressBar.visibility = View.VISIBLE
+            findNavController().navigate(ConfirmationFragmentDirections.actionConfirmationFragmentToTemplateFragment())
+            binding.progressBar.visibility = View.GONE
+        }
 
         binding.confirmButton.setOnClickListener {
             viewModel.sendDocumentForSignature(requestBody) //sending request body to Post request
