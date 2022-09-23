273650a6549e743901842ccaf420ed282e771c07
itsnitish22 <nitish.sharma1186@gmail.com>
2022-09-23 21:49:07 +0530

    i swear i told aniket not to mess up with themes calendar returned using inline, date selection color to be changed (primary color)

--- app/src/main/java/com/teamdefine/signease/DatePicker.kt
+++ app/src/main/java/com/teamdefine/signease/DatePicker.kt
@@ -5,18 +5,14 @@ import com.google.android.material.datepicker.DateValidatorPointForward
 import com.google.android.material.datepicker.MaterialDatePicker
 
 class DatePicker {
-
     fun getCalendar(date: Long): MaterialDatePicker<Long> {
-//        val today=date.time
         val constraintsBuilder =
             CalendarConstraints.Builder()
                 .setValidator(DateValidatorPointForward.now())
-        val datePicker =
-            MaterialDatePicker.Builder.datePicker()
-                .setTitleText("Select date")
-                .setSelection(date)
-                .setCalendarConstraints(constraintsBuilder.build())
-                .build()
-        return datePicker
+        return MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select date")
+            .setSelection(date)
+            .setCalendarConstraints(constraintsBuilder.build())
+            .build()
     }
 }
