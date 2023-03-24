de82a03d07eb6a89f01eb112e2752f323907c05d
Nitish Sharma <nitish.sharma1186@gmail.com>
2023-03-24 19:48:39 +0530

    [feat] forgot password

--- app/src/main/java/com/teamdefine/signease/loginandregister/LoginFragment.kt
+++ app/src/main/java/com/teamdefine/signease/loginandregister/LoginFragment.kt
@@ -10,13 +10,13 @@ import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.teamdefine.signease.databinding.FragmentLoginBinding
 
+
 class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding //binding
     private lateinit var firebaseAuth: FirebaseAuth //firebase auth
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         firebaseAuth = FirebaseAuth.getInstance() //getting instance
@@ -26,8 +26,11 @@ class LoginFragment : Fragment() {
             val email = binding.inputEmail.text.toString()
             val password = binding.inputPassword.text.toString()
 
-            if (email.isEmpty() || password.isEmpty())
-                Toast.makeText(activity, "All fields are mandatory", Toast.LENGTH_SHORT).show()
+            if (email.isEmpty() || password.isEmpty()) Toast.makeText(
+                activity,
+                "All fields are mandatory",
+                Toast.LENGTH_SHORT
+            ).show()
             else {
                 binding.progressBar.visibility = View.VISIBLE
                 loginUser(email, password)
@@ -36,26 +39,53 @@ class LoginFragment : Fragment() {
         binding.signUp.setOnClickListener {
             findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment())
         }
+
+        binding.forgotPassword.setOnClickListener {
+            if (binding.inputEmail.text.toString().isEmpty()) Toast.makeText(
+                requireContext(),
+                "Enter email first",
+                Toast.LENGTH_SHORT
+            ).show()
+            else {
+                binding.progressBar.visibility = View.VISIBLE
+                sendResetPasswordEmail(binding.inputEmail.text.toString())
+            }
+        }
+
         return binding.root
+    }
+
+    private fun sendResetPasswordEmail(email: String) {
+        firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                binding.progressBar.visibility = View.GONE
+                Toast.makeText(
+                    requireContext(), "Reset password email sent", Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                binding.progressBar.visibility = View.GONE
+                Toast.makeText(
+                    requireContext(),
+                    "Some internal error occurred. Try again later.",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 
     //login user function
     private fun loginUser(email: String, password: String) {
-        firebaseAuth.signInWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    binding.progressBar.visibility = View.GONE
-                    Toast.makeText(activity, "Logged In", Toast.LENGTH_SHORT)
-                        .show()
-                    findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToHomePageFragment())
-                } else {
-                    binding.progressBar.visibility = View.GONE
-                    Toast.makeText(
-                        activity,
-                        task.exception!!.message.toString(),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+        firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                binding.progressBar.visibility = View.GONE
+                Toast.makeText(activity, "Logged In", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToHomePageFragment())
+            } else {
+                binding.progressBar.visibility = View.GONE
+                Toast.makeText(
+                    activity, task.exception!!.message.toString(), Toast.LENGTH_SHORT
+                ).show()
             }
+        }
     }
 }
