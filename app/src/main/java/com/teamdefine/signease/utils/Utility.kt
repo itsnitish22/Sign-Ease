25e9895d26a2f48db4108ef00440925248883ae4
aniketk13 <ani.khajanchi257@gmail.com>
2023-04-09 21:44:31 +0530

    download complete notification visibility

--- app/src/main/java/com/teamdefine/signease/utils/Utility.kt
+++ app/src/main/java/com/teamdefine/signease/utils/Utility.kt
@@ -1,13 +1,19 @@
 package com.teamdefine.signease.utils
 
 import android.app.DownloadManager
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.net.Uri
 import android.os.Environment
+import android.util.Log
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 object Utility {
     fun Context.toast(message: String) {
@@ -27,9 +33,10 @@ object Utility {
         request.setTitle(fileTitle)
         request.setDescription("Downloading")
         request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
         downloadManager.enqueue(request)
-    }
 
+    }
     fun Fragment.downloadFile(fileUrl: String, fileTitle: String, fileName: String) {
         requireContext().downloadFile(fileUrl, fileTitle, fileName)
     }
