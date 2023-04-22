af76913df72b9dc25a637d1703e4cb703c644203
Nitish Sharma <nitish.sharma1186@gmail.com>
2023-04-22 14:16:20 +0530

    [cleanup] created urls object

---
+++ data/src/main/java/com/teamdefine/data/constants/Urls.kt
@@ -0,0 +1,10 @@
+package com.teamdefine.data.constants
+
+object Urls {
+    const val GET_ALL_SIGNATURE_REQUESTS = "/v3/signature_request/list"
+    const val SEND_WITH_TEMPLATE_FOR_SIGNATURE = "/v3/signature_request/send_with_template"
+    const val GET_ALL_TEMPLATES_LIST = "/v3/template/list"
+    const val GET_FILE_URL_FOR_DOWNLOAD = "v3/signature_request/files"
+    const val DELETE_SIGNATURE_REQUEST = "v3/signature_request/cancel"
+    const val HELLO_SIGN_APP = "v3/api_app"
+}
